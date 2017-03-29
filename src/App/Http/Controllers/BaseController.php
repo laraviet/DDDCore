4c5fc3da47caef3cf19fdc86f1184eb077f3c6eb
Nguyen Trung Thanh <thanhnt@portalbeanz.com>
2017-03-29 17:48:45 +0700

    Add BaseController

---
+++ src/App/Http/Controllers/BaseController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Laraviet\DDDCore\App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class BaseController extends Controller
+{
+    protected $service;
+    protected $package;
+
+    public function index()
+    {
+        $books = $this->service->paginate();
+        return view("{$this->package}::books.index", compact("books"));
+    }
+
+    public function create()
+    {
+        return view("{$this->package}::books.create");
+    }
+
+    public function store(Request $request)
+    {
+        $this->service->persist($request);
+        return redirect()->route('books.index');
+    }
+
+    public function show($id)
+    {
+        $book = $this->service->getById($id);
+        return view("{$this->package}::books.show", compact("book"));
+    }
+
+    public function edit($id)
+    {
+        $book = $this->service->getById($id);
+        return view("{$this->package}::books.edit", compact("book"));
+    }
+
+    public function update($id, Request $request)
+    {
+        $this->service->persist($request, $id);
+        return redirect()->route('books.index');
+    }
+
+    public function destroy($id)
+    {
+        $this->service->destroy($id);
+        return redirect()->route('books.index');
+    }
+}
